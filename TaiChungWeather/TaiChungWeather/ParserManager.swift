fb6f58aac253d528f75994d18cdebc1f765ed445
Jerome Hsieh <jerome.developer.tw@gmail.com>
2019-09-05 22:04:25 +0800

    1.刪除多餘的程式。

--- TaiChungWeather/TaiChungWeather/ParserManager.swift
+++ TaiChungWeather/TaiChungWeather/ParserManager.swift
@@ -11,68 +11,10 @@ import Ji
 
 class ParserManager {
   static let shared = ParserManager()  // Singleton
-//  public var didQueryWeatherHandler: DoneHandler?
-  
-  private let feedWeatherURLString = "http://www.cwb.gov.tw/rss/forecast/36_08.xml"
   
   private init() {
   }
-  
-  enum XMLKey: String {
-    case item, pubDate, description
-  }
-  
-  
-  /// Parse weather XML string.
-  ///
-  /// - Parameter xmlString: XML string.
-  /// - Returns: Return true means success.
-  public func parseWeatherXML(xmlString: String) -> Bool {
-    /*
-     The path to pubdate and week weather description:
-       <rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/">  // Root Node
-       <channel version="2.0">
-       <item>
-       [1]
-       <pubdate> AND <description>[0]
-     */
     
-    guard let jiDoc = Ji(xmlString: xmlString), let targetItemNode = jiDoc.rootNode?.firstChild?.xPath(XMLKey.item.rawValue)[1] else {
-      return false
-    }
-    guard let pubDateString = targetItemNode.xPath(XMLKey.pubDate.rawValue).first?.content, let weekWeatherString = targetItemNode.xPath(XMLKey.description.rawValue).first?.content else {
-      return false
-    }
-    
-    let myContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    let coreDataConnect = CoreDataConnect(context: myContext)
-    
-    guard let date = Date.dateFromString(dateTimeString: pubDateString) else {
-      assertionFailure()
-      return false
-    }
-    
-    let temp = NSPredicate(format: "time = %@", date as CVarArg)
-    guard let results = coreDataConnect.retrieveWeekWeatherResults(predicate: temp, sort: nil, limit: 1), results.isEmpty else {
-      DLog("The record is already existed.")
-      return true
-    }
-    
-    // insert
-    let insertResult = coreDataConnect.insert(
-      Constant.weatherEntityName, attributeInfo: [
-        Constant.timeKey : date as Any,
-        Constant.contentKey : weekWeatherString as Any
-      ])
-    if insertResult {
-      DLog("Insert successfully.")
-      return true
-    } else {
-      DLog("Insert failed.")
-      return false
-    }
-  }
-  
   /// Parse daily quote HTML string.
   ///
   /// - Parameter htmlString: HTML string.
